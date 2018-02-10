e53f5c90a736205d21810aed03c269632ec98a6c
MusicGTELife <musicgtelife@gmail.com>
2018-02-09 21:10:45 -0600

    fixed ethereal typo

--- monsterclass.js
+++ monsterclass.js
@@ -2,7 +2,7 @@ const MonsterClass = { }
 MonsterClass.UNDEAD =           0x01
 MonsterClass.BEAST =            0x02
 MonsterClass.HUMANOID =         0x03
-MonsterClass.AETHERIAL =        0x04
+MonsterClass.ETHEREAL =        0x04
 MonsterClass.DEMON =            0x05
 
 const UndeadClass = { }
@@ -20,14 +20,14 @@ HumanoidClass.BANDIT =          0x01
 HumanoidClass.TAKEN =           0x02
 HumanoidClass.DARKSWORN =       0x03
 
-const EtherialClass = { }
-EtherialClass.SPRITE =         0x01
-EtherialClass.GHOST =          0x02
-EtherialClass.WIZARD =         0x03
+const EtherealClass = { }
+EtherealClass.SPRITE =         0x01
+EtherealClass.GHOST =          0x02
+EtherealClass.WIZARD =         0x03
 
 const DemonClass = { }
 DemonClass.SERVANT =            0x01
 DemonClass.GHOUL =              0x02
 DemonClass.COMMANDER =          0x03
 
-module.exports = { MonsterClass, UndeadClass, BeastClass, HumanoidClass, EtherialClass, DemonClass }
+module.exports = { MonsterClass, UndeadClass, BeastClass, HumanoidClass, EtherealClass, DemonClass }
