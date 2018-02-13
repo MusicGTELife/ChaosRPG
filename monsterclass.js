5f6d63ee1e1971810609b8e69a3c8ca8ef9a57c2
bsdunx <bsdunx@gmail.com>
2018-02-13 00:47:52 -0500

    Add monster classes into the

--- monsterclass.js
+++ monsterclass.js
@@ -2,7 +2,7 @@ const MonsterClass = { }
 MonsterClass.UNDEAD =           0x01
 MonsterClass.BEAST =            0x02
 MonsterClass.HUMANOID =         0x03
-MonsterClass.ETHEREAL =        0x04
+MonsterClass.ETHEREAL =         0x04
 MonsterClass.DEMON =            0x05
 
 const UndeadClass = { }
@@ -21,13 +21,16 @@ HumanoidClass.TAKEN =           0x02
 HumanoidClass.DARKSWORN =       0x03
 
 const EtherealClass = { }
-EtherealClass.SPRITE =         0x01
-EtherealClass.GHOST =          0x02
-EtherealClass.WIZARD =         0x03
+EtherealClass.SPRITE =          0x01
+EtherealClass.GHOST =           0x02
+EtherealClass.WIZARD =          0x03
 
 const DemonClass = { }
 DemonClass.SERVANT =            0x01
 DemonClass.GHOUL =              0x02
 DemonClass.COMMANDER =          0x03
 
-module.exports = { MonsterClass, UndeadClass, BeastClass, HumanoidClass, EtherealClass, DemonClass }
+module.exports = {
+    MonsterClass,
+    UndeadClass, BeastClass, HumanoidClass, EtherealClass, DemonClass
+}
