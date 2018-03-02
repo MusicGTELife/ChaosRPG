2e4d65669c30f04ad62e407289eb60ba2a4f408f
bsdunx <bsdunx@gmail.com>
2018-03-02 02:24:52 -0500

    Turn monster class ids into flags

--- monsterclass.js
+++ monsterclass.js
@@ -1,8 +1,8 @@
 const MonsterClass = { }
 MonsterClass.UNDEAD =           0x01
-MonsterClass.BEAST =            0x02
-MonsterClass.HUMANOID =         0x03
-MonsterClass.ETHEREAL =         0x04
-MonsterClass.DEMON =            0x05
+MonsterClass.BEAST =            0x01 << 1
+MonsterClass.HUMANOID =         0x01 << 2
+MonsterClass.ETHEREAL =         0x01 << 3
+MonsterClass.DEMON =            0x01 << 4
 
 module.exports = { MonsterClass }
