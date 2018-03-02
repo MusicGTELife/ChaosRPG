6008b929c68fb98f72c2ade23865b678e1574534
bsdunx <bsdunx@gmail.com>
2018-03-02 00:04:22 -0500

    Item mod updates - tweak mod ranges

--- itemmod.js
+++ itemmod.js
@@ -16,19 +16,19 @@ const _ = ItemModTable.createEntry
 
 ItemModTable.INT = _(
     0x0001, 'intelligence', ItemModClass.MINOR.id,
-    [ { 'id': StatTable.INT.id, 'min_value': 1, 'max_value': 5 } ]
+    [ { 'id': StatTable.INT.id, 'min_value': 3, 'max_value': 6 } ]
 )
 ItemModTable.DEX = _(
     0x0002, 'dexterity', ItemModClass.MINOR.id,
-    [ { 'id': StatTable.DEX.id, 'min_value': 1, 'max_value': 5 } ]
+    [ { 'id': StatTable.DEX.id, 'min_value': 3, 'max_value': 6 } ]
 )
 ItemModTable.STR = _(
     0x0003, 'strength', ItemModClass.MINOR.id,
-    [ { 'id': StatTable.STR.id, 'min_value': 1, 'max_value': 5 } ]
+    [ { 'id': StatTable.STR.id, 'min_value': 3, 'max_value': 6 } ]
 )
 ItemModTable.VIT = _(
     0x0004, 'vitality', ItemModClass.MINOR.id,
-    [ { 'id': StatTable.VIT.id, 'min_value': 1, 'max_value': 5 } ]
+    [ { 'id': StatTable.VIT.id, 'min_value': 3, 'max_value': 6 } ]
 )
 
 ItemModTable.ALL_ATTR = _(
@@ -38,24 +38,24 @@ ItemModTable.ALL_ATTR = _(
 
 ItemModTable.ATK = _(
     0x0100, 'attack', ItemModClass.MINOR.id,
-    [ { 'id': StatTable.ATK.id, 'min_value': 1, 'max_value': 10 } ]
+    [ { 'id': StatTable.ATK.id, 'min_value': 4, 'max_value': 10 } ]
 )
 ItemModTable.MATK = _(
     0x0101, 'force', ItemModClass.MINOR.id,
-    [ { 'id': StatTable.MATK.id, 'min_value': 1, 'max_value': 10 } ]
+    [ { 'id': StatTable.MATK.id, 'min_value': 4, 'max_value': 10 } ]
 )
 ItemModTable.DEF = _(
     0x0102, 'defense', ItemModClass.MINOR.id,
-    [ { 'id': StatTable.DEF.id, 'min_value': 1, 'max_value': 10 } ]
+    [ { 'id': StatTable.DEF.id, 'min_value': 4, 'max_value': 10 } ]
 )
 ItemModTable.MDEF = _(
     0x0103, 'resistance', ItemModClass.MINOR.id,
-    [ { 'id': StatTable.MDEF.id, 'min_value': 1, 'max_value': 10 } ]
+    [ { 'id': StatTable.MDEF.id, 'min_value': 4, 'max_value': 10 } ]
 )
 
 ItemModTable.HP = _(
     0x0110, 'hit points', ItemModClass.MINOR.id,
-    [ { 'id': StatTable.HP.id, 'min_value': 1, 'max_value': 10 } ]
+    [ { 'id': StatTable.HP.id, 'min_value': 5, 'max_value': 15 } ]
 )
 
 ItemModTable.ACCURACY = _(
@@ -65,7 +65,7 @@ ItemModTable.ACCURACY = _(
 
 ItemModTable.REACTION = _(
     0x0121, 'reaction', ItemModClass.MINOR.id,
-    [ { 'id': StatTable.REACTION.id, 'min_value': 1, 'max_value': 10 } ]
+    [ { 'id': StatTable.REACTION.id, 'min_value': 2, 'max_value': 10 } ]
 )
 
 module.exports = { ItemModClass, ItemModTable }
